c4f1f6f5e2bb1650787f3b3508485f6cb4b37094
G93-32 <Singh.Gurjot@strathmore.edu>
2025-01-23 09:39:46 +0300

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,24 +1,5 @@
-package first;
+package Lesson;
 
 public class Employee {
-String name;
-String department;
-String position;
-long salary;
-long phoneNumber;
-void receiveSalary() {
-	System.out.println("Receive salary at the end of every month.\n");
-}
-void quota() {
-	System.out.println("Make sure the quota is followed to avoid any fines.\n");
-}
-void punctuality() {
-	System.out.println("Make sure to be punctual to avoid being fined");
-}
-public static void main(String[] args) {
-	Employee Gurjot = new Employee();
-	Gurjot.receiveSalary();
-	Gurjot.quota();
-	Gurjot.punctuality();
-}
+
 }
